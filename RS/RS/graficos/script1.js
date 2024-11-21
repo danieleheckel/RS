0e0370f274e20e7e704665cdb9018dd6a2b73643
Amayya <105234629+danieleheckel@users.noreply.github.com>
2024-11-21 09:52:20 -0300

    Add files via upload

---
+++ RS/RS/graficos/script1.js
@@ -0,0 +1,33 @@
+import { getCSS } from "./script2"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--cor-3')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--color-5'),
+        paper_bgcolor: getCSS('--color-1')
+
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuariosPorRede()
